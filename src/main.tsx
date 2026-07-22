9d32faefb7ac7da5e81bf05eab315735f5f24cd4
gautham-websters <gautham.webster@gmail.com>
2026-07-22 14:29:21 +0400

    fix: static build

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { getRouter } from "./router";
+import "./styles.css"; // Import your global CSS here
+
+const rootElement = document.getElementById("root")!;
+
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={getRouter()} />
+    </React.StrictMode>,
+  );
+}
